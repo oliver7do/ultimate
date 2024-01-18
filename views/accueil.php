de452de2d26d2cab7a613c52f5ec110aac9b5014
Oliver <afristreaming@gmail.com>
2024-01-18 07:48:30 +0100

    1 versao

---
+++ views/accueil.php
@@ -0,0 +1,57 @@
+<div class="content">
+<div class="first">
+    <div class="first1">
+        <div class="first12">
+            <div class="first121">
+                <p>#informations</p>
+            </div>
+            <div class="first1211">
+            <div class="first12111">
+            <h1>Site web de <br> communication scolaire</h1>
+            </div>
+            <div class="first121112">
+            <p>Avoir le plaisir de faire partie de cette <br> petite communauté, où seuls les élèves <br> de notre école ont leur place.</p>
+            </div>
+            <div class="bts"><a>Commencer gratuitement aujourd'hui</a>
+                <div class="bts1">
+                <i class="fa-solid fa-arrow-right"></i>
+                </div>
+            </div>
+            </div>
+             </div>
+        <div class="first13">
+        <img src="../../ultimate/views/img/1photo.jpg" width="670"
+  height="490">
+        </div>     
+    </div>
+</div>
+
+<div class="prix">
+    <div class="prix1">
+        <h3>Gratuit</h3>
+        <h1>GRATUIT</h1>
+        <p>gratuit</p>
+
+        <div class="check">
+            <div class="check0">
+            <i class="fa-solid fa-check"></i>
+            <p>Accés limité</p>
+            </div>
+            <div class="check00">
+            <i class="fa-solid fa-check"></i>
+            <p>Chat gratuit</p>
+        </div>
+        </div>
+
+    </div>
+    <div class="prix2">
+
+    </div>
+    <div class="prix3">
+
+    </div>
+</div>
+
+
+</div>
+
---
+++ views/nav.php
@@ -0,0 +1,54 @@
+<div class="nav1">
+
+<ul class="nav justify-content-center">
+  <div class="logolink">
+  <a class="nav-link active" href="#">Apprenect</a>
+  </div>
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Important</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Prix</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">A propos de nous</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Connexion</a>
+  </li>
+  <div class="bot1">
+  <a class="nav-link active" href="#">S'inscrire</a>
+  <i class="fa-solid fa-arrow-right"></i>
+</div>
+</ul>
+</div>
+
+    
+   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</div>
